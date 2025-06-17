0f284dccc02a6be6e19129e7e99e226ca3a3d401
Jack van den Berg <your-email@example.com>
2025-06-17 13:48:50 +0200

    minor refactor

--- server/index.js
+++ server/index.js
@@ -1,22 +1,18 @@
-const { createServer } = require('http');
-const { Server } = require('socket.io');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*" // Be careful with this in production
-  }
-});
+const server = createServer();
 
-io.on('connection', (socket) => {
-  console.log('A client connected');
+const io = new Server(server);
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+io.on("connection", (socket) => {
+  console.log("A client connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
 
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
