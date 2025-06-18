01e3f37c2718734fbb8a47ad7fd8f54676206a1f
Jack van den Berg <your-email@example.com>
2025-06-18 11:36:39 +0200

    Remove centroid logic

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -6,9 +6,7 @@ db.serialize(() => {
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     count INTEGER NOT NULL,
     timestamp DATETIME NOT NULL,
-    image TEXT,
-    centroid_x INTEGER,
-    centroid_y INTEGER
+    image TEXT
   )`);
 });
 
