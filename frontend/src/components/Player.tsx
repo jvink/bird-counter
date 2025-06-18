7d9f924a23c92a3276303a955275ad16f818e654
Jurian <ext-jurian.vink@bitvavo.com>
2025-06-18 12:42:16 +0200

    socket to app

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,12 +1,9 @@
-import { createSignal, onMount, onCleanup } from "solid-js";
-import { io } from "socket.io-client";
+import { createSignal } from "solid-js";
 import MiniPlayer from "./MiniPlayer";
 import LargePlayer from "./LargePlayer";
 
-export default function Player() {
+export default function Player(props: { birdData: { count: number; timestamp: string; image: string } | null }) {
     const [isOpen, setIsOpen] = createSignal(false);
-    const [birdData, setBirdData] = createSignal<{ count: number; timestamp: string; image: string } | null>(null);
-    let socket: any;
 
     const openPlayer = () => {
         setIsOpen(true);
@@ -16,29 +13,10 @@ export default function Player() {
         setIsOpen(false);
     };
 
-    onMount(() => {
-        socket = io("http://192.168.178.135:3000");
-        
-        socket.on('connect', () => {
-            console.log('Player connected to server');
-        });
-
-        socket.on('bird_update', (data: { count: number; timestamp: string; image: string }) => {
-            console.log('Player received bird_update:', data);
-            setBirdData(data);
-        });
-    });
-
-    onCleanup(() => {
-        if (socket) {
-            socket.disconnect();
-        }
-    });
-
     return (
         <>
-            {!isOpen() && <MiniPlayer onOpen={openPlayer} birdData={birdData()} />}
-            {isOpen() && <LargePlayer onClose={closePlayer} birdData={birdData()} />}
+            {!isOpen() && <MiniPlayer onOpen={openPlayer} birdData={props.birdData} />}
+            {isOpen() && <LargePlayer onClose={closePlayer} birdData={props.birdData} />}
         </>
     );
 }
